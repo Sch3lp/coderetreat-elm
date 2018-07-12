7bce9b2e3a0306fbbfa78c23ba439762a7257d21
sch3lp <sch3lp@gmail.com>
2018-07-12 12:26:48 +0200

    extracted moveAndWrap out of test

--- src/Main.elm
+++ src/Main.elm
@@ -31,6 +31,10 @@ type alias Planet =
     }
 
 
+type alias PlanetPos =
+    { pos : Pos, planet : Planet }
+
+
 initialRover : Rover
 initialRover =
     Rover North <| Pos 0 0
@@ -121,6 +125,42 @@ moveX dir pos =
     { pos | x = pos.x + (dir 1) }
 
 
+moveXAndWrap : AxisDirection -> PlanetPos -> PlanetPos
+moveXAndWrap dir planetPos =
+    let
+        currentPos =
+            planetPos.pos
+
+        moveInDir =
+            moveX dir
+
+        newPos =
+            if wouldMoveOutOfBounds moveInDir planetPos then
+                { currentPos | x = (currentPos.x * -1) }
+            else
+                moveInDir currentPos
+    in
+        { planetPos | pos = newPos }
+
+
+moveYAndWrap : AxisDirection -> PlanetPos -> PlanetPos
+moveYAndWrap dir planetPos =
+    let
+        currentPos =
+            planetPos.pos
+
+        moveInDir =
+            moveY dir
+
+        newPos =
+            if wouldMoveOutOfBounds moveInDir planetPos then
+                { currentPos | y = (currentPos.y * -1) }
+            else
+                moveInDir currentPos
+    in
+        { planetPos | pos = newPos }
+
+
 
 -- turning
 
@@ -183,3 +223,16 @@ edges p =
             Pos (edge) (inverse edge)
     in
         [ edge1, edge2, edge3, edge4 ]
+
+
+wouldMoveOutOfBounds : (Pos -> Pos) -> PlanetPos -> Bool
+wouldMoveOutOfBounds moveDir planetPos =
+    let
+        currentPos =
+            planetPos.pos
+
+        newPos =
+            moveDir currentPos
+    in
+        edges planetPos.planet
+            |> List.any (\edgePos -> ((abs newPos.x) > (abs edgePos.x)) || ((abs newPos.y) > (abs edgePos.y)))
